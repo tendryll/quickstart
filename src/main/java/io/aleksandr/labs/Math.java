0236bff41aa96c381091edef9aadde6131e4116a
Sean Alphonse <sean@aleksandr.cc>
2020-10-18 14:24:04 -0500

    Added subtract method and corresponding tests.

--- src/main/java/io/aleksandr/labs/Math.java
+++ src/main/java/io/aleksandr/labs/Math.java
@@ -4,4 +4,8 @@ public interface Math {
   static int add(final int x, final int y) {
     return x + y;
   }
+
+  static int subtract(final int x, final int y) {
+    return x - y;
+  }
 }
--- src/test/java/io/aleksandr/labs/MathTest.java
+++ src/test/java/io/aleksandr/labs/MathTest.java
@@ -11,4 +11,10 @@ class MathTest {
     final int sum = Math.add(1, 1);
     assertEquals(2, sum);
   }
+
+  @Test
+  void subtract() {
+    final int difference = Math.subtract(2, -1);
+    assertEquals(3, difference);
+  }
 }
